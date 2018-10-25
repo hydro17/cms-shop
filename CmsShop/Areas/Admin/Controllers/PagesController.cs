e5416728e65b75524756663fc0adae544732700f
Andrzej Pepłowski <pirx555@gmail.com>
2018-10-25 16:12:21 +0200

    Update Admin.Controllers.Pages.Index. Add Admin.Views.Pages.Index.

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace CmsShop.Areas.Admin.Controllers
 {
@@ -7,7 +11,14 @@ namespace CmsShop.Areas.Admin.Controllers
     // GET: Admin/Pages
     public ActionResult Index()
     {
-      return View();
+      List<PageVM> pagesList;
+
+      using (Db db = new Db())
+      {
+        pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+      }
+
+      return View(pagesList);
     }
   }
 }
